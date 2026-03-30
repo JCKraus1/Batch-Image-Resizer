cd9dfae4d1b567a594117d3ec43bce456301a536
JCKraus1 <jc.kraus1@gmail.com>
2026-03-30 09:44:29 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -164,7 +164,7 @@ export default function App() {
         <div className="max-w-5xl mx-auto px-6 py-4 flex items-center justify-between">
           <div className="flex items-center gap-3">
             <img 
-              src="https://jckraus1.github.io/Batch-Image-Resizer/LSCG_LOGO_B&W.png" 
+              src="./LSCG_LOGO_B&W.png" 
               alt="LSCG Logo" 
               className="h-12 w-auto object-contain"
               referrerPolicy="no-referrer"
